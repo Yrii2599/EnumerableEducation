3773258b148cb64bf8d8e79be7e2c33b4f419748
Юрій Бондаренко <yurii.bondarenko@nure.ua>
2021-05-17 22:57:55 +0300

    Добавьте файлы проекта.

---
+++ EnumerableEducation/MyType.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableEducation
+{
+    public class Car
+    {
+        // Константа для представления максимальной скорости,
+        public const int MaxSpeed = 100;
+        // Свойства автомобиля.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = " ";
+        //He вышел ли автомобиль из строя?
+        private bool carlsDead;
+        //В автомобиле имеется радиоприемник,
+        private Radio theMusicBox = new Radio();
+        // Конструкторы,
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            // Делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+        }
+        // Проверить, не перегрелся ли автомобиль,
+        public void Accelerate(int delta)
+        {
+            if (carlsDead)
+            {
+                Console.WriteLine("{0} is out of order...", PetName);
+            }
+            else
+                CurrentSpeed += delta;
+            if (CurrentSpeed > MaxSpeed)
+            {
+                Console.WriteLine("{0} has overheated!", PetName);
+                CurrentSpeed = 0;
+                carlsDead = true;
+            }
+            else
+                Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+        }
+        public class Garage : IEnumerable
+        {
+            private Car[] carArray = new Car[4];
+            // При запуске заполнить несколькими объектами Саг.
+            public Garage()
+            {
+                carArray[0] = new Car("Rusty", 30);
+                carArray[1] = new Car("Clunker", 55);
+                carArray[2] = new Car("Zippy", 30);
+                carArray[3] = new Car("Fred", 30);
+            }
+            public IEnumerator GetEnumerator()
+            {
+               // throw new Exception("excaption");
+                return actualImplementation();
+                IEnumerator actualImplementation()
+                {
+
+                    foreach (Car c in carArray)
+                    {
+                        yield return c;
+                    }
+                }
+            }
+        }   
+        class Radio
+        {
+            public void TurnOn(bool on)
+            {
+                Console.WriteLine(on ? "Jamming..." : "Quiet time...");
+            }
+        }
+    }
+}
---
+++ EnumerableEducation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static EnumerableEducation.Car;
+using System.Collections;
+
+namespace EnumerableEducation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 50 };
+            Garage g = new Garage();
+            foreach(Car i in g)
+            {
+                Console.WriteLine(i.PetName);
+            }
+            IEnumerator enumerator = g.GetEnumerator();
+        }
+    }
+}
